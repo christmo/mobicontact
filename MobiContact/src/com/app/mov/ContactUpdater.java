c1d0bd4bf22deb9ae9b1151f078c9461d6139734
christmo99 <christmo99@e6710fb3-1049-0e57-c732-339528bdb854>
2012-08-24 04:33:41 +0000

    cambio para que no salga de la aplicacion al retroceder

--- MobiContact/src/com/app/mov/ContactUpdater.java
+++ MobiContact/src/com/app/mov/ContactUpdater.java
@@ -1,16 +1,28 @@
 package com.app.mov;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import org.apache.cordova.DroidGap;
 
-public class ContactUpdater extends DroidGap
-{
-    /** Called when the activity is first created. */
+public class ContactUpdater extends DroidGap {
+
+    /**
+     * Called when the activity is first created.
+     */
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         super.loadUrl("file:///android_asset/www/index.html");
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            if (appView.canGoBack()) {
+                appView.goBack();
+                return true;
+            }
+        }
+        return super.onKeyDown(keyCode, event);
     }
 }
