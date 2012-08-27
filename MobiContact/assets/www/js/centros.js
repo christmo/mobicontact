e69c3a8a3d65b823c435ccd34f2661c903c596ca
ysp80s <ysp80s@e6710fb3-1049-0e57-c732-339528bdb854>
2012-08-27 21:52:06 +0000

    Centros obtenidos del servidor y informacion de locales extraida geo en 100%

--- MobiContact/assets/www/js/centros.js
+++ MobiContact/assets/www/js/centros.js
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-var ciudadActual;
+
 var ciudades = [];
 
 $("#centros").live("pageshow", function(prepage) {
@@ -14,23 +14,55 @@ $("#centros").live("pageshow", function(prepage) {
 $("#centros ul li").live("click",function(evt){
     indActual = $(this).attr("id");
     $.mobile.changePage("#locales","slide",false,true);
-    evt.preventDefault();
+    
 });
 
 $("#locales").live("pageshow",function(){
-    
+    $(".inner").html("<p>"+ciudades[indActual].nombre+"</p>");
     var localesHtml = "";
     for(var i = 0; i<ciudades[indActual].locales.length;i++){
+        var mlocal = ciudades[indActual].locales[i];
+       
         localesHtml += "<li id=\""+i+"\" data-wrapperels=\"div\" data-icon=\"arrow-r\" data-iconpos=\"right\""+
-                    " data-theme=\"a\" class=\"ui-btn ui-btn-icon-right ui-li-has-arrow ui-li ui-btn-up-c\">"+
-                    "<div class=\"ui-btn-inner ui-li\"><div class=\"ui-btn-text\"><a href=\"#\""+
-                    "class=\"ui-link-inherit\">"+ciudades[indActual].locales[i].key+"</a></div>"+
-                    "<span class=\"ui-icon ui-icon-arrow-r ui-icon-shadow\">&nbsp;</span></div></li>";
+        " class=\"ui-btn ui-btn-icon-right ui-li-has-arrow ui-li ui-btn-up-c\">"+
+        "<div class=\"ui-btn-inner ui-li\"><div class=\"ui-btn-text\"><a href=\"#\""+
+        "class=\"ui-link-inherit\">"+mlocal.nombre+"</a></div>"+
+        "<span class=\"ui-icon ui-icon-arrow-r ui-icon-shadow\">&nbsp;</span></div></li>";
     }
     $(".locales").html(localesHtml);
 });
     
-   
+$("#locales div li").live("click",function(evt){
+    indLocal = $(this).attr("id");
+    $.mobile.changePage("#mapa","slide",false,true);
+    
+}); 
+
+
+$("#mapa").live("pageshow",function(){
+    $(".nombre_local").html("");
+    $(".nombre_local").html("<h4>"+ciudades[indActual].locales[indLocal].nombre+"</h4>");
+    init(ciudades[indActual].locales[indLocal].coordenadas);
+    
+});
+
+
+function init(position){
+    document.getElementById("map").innerHTML = "";
+    var coordenadas = position.split(",");
+    map = new OpenLayers.Map( 'map' );
+    map.addLayer(new OpenLayers.Layer.OSM());
+    ourpoint = new OpenLayers.LonLat(  coordenadas[1],coordenadas[0]);
+    ourpoint.transform(new OpenLayers.Projection("EPSG:4326" ), map.getProjectionObject());
+    map.setCenter(ourpoint, 17);
+                
+    var zoom=16;
+    var markers = new OpenLayers.Layer.Markers( "Markers" );
+    map.addLayer(markers); 
+    markers.addMarker(new OpenLayers.Marker(ourpoint)); 
+    map.setCenter (ourpoint, zoom);
+    
+}
 
 
 
@@ -42,15 +74,15 @@ function getJson(){
         async: false,
         dataType: "json",
         success: function(data){
-                 ciudades = data.ciudades;       
+            ciudades = data.ciudades;       
             for(var i = 0; i < data.ciudades.length;i++ ){
                 document.getElementById("centros_div").innerHTML +=
                     
-                    "<li id=\""+i+"\" data-wrapperels=\"div\" data-icon=\"arrow-r\" data-iconpos=\"right\""+
-                    " data-theme=\"a\" class=\"ui-btn ui-btn-icon-right ui-li-has-arrow ui-li ui-btn-up-c\">"+
-                    "<div class=\"ui-btn-inner ui-li\"><div class=\"ui-btn-text\"><a href=\"#\""+
-                    "class=\"ui-link-inherit\">"+data.ciudades[i].nombre+"</a></div>"+
-                    "<span class=\"ui-icon ui-icon-arrow-r ui-icon-shadow\">&nbsp;</span></div></li>";
+                "<li id=\""+i+"\" data-wrapperels=\"div\" data-icon=\"arrow-r\" data-iconpos=\"right\""+
+                " class=\"ui-btn ui-btn-icon-right ui-li-has-arrow ui-li ui-btn-up-c\">"+
+                "<div class=\"ui-btn-inner ui-li\"><div class=\"ui-btn-text\"><a href=\"#\""+
+                "class=\"ui-link-inherit\">"+data.ciudades[i].nombre+"</a></div>"+
+                "<span class=\"ui-icon ui-icon-arrow-r ui-icon-shadow\">&nbsp;</span></div></li>";
                   
                             
             }
