30db3247a688465e5ae4d78c777bfb75912df021
christmo99 <christmo99@e6710fb3-1049-0e57-c732-339528bdb854>
2012-09-02 05:45:39 +0000

    base de datos

--- zeus/js/crear.js
+++ zeus/js/crear.js
@@ -1,15 +1,19 @@
-var position;
+var map_1;
 
 $("#crear").live("pageshow",function(){
-    $('#i_lat').textinput('disable');
-    $('#i_lon').textinput('disable');
-    mapa(position);
+//    $('#i_lat').textinput('disable');
+//    $('#i_lon').textinput('disable');
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(success, error);
+    } else {
+        error('not supported');
+    }
 });
 
 function success(position) {
     $('#i_lat').attr('value',position.coords.latitude);
     $('#i_lon').attr('value',position.coords.longitude);
-    this.position = position;
+    mapa(position);
 }
 
 function error(msg) {
@@ -18,22 +22,29 @@ function error(msg) {
     $('#i_lon').textinput('enable');
 }
 
-if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(success, error);
-} else {
-    error('not supported');
-}
-
 function mapa(position){
     document.getElementById("map_1").innerHTML = "";
-    crearMapa('map_1');
-//                
-//    var ourpoint = new OpenLayers.LonLat(  position.coords.latitude,position.coords.longitude);
-//    ourpoint.transform(new OpenLayers.Projection("EPSG:4326" ), map.getProjectionObject());
-//    map.setCenter(ourpoint, 17); 
+        map_1 = new OpenLayers.Map( 'map_1' );
+        map_1.addLayer(new OpenLayers.Layer.OSM());
+        var ourpoint = new OpenLayers.LonLat(position.coords.longitude,position.coords.latitude);
+        ourpoint.transform(new OpenLayers.Projection("EPSG:4326" ), map.getProjectionObject());
+        map_1.setCenter(ourpoint, 17);
+                    
+        var zoom=16;
+        var markers = new OpenLayers.Layer.Markers( "Markers" );
+        map_1.addLayer(markers); 
+        markers.addMarker(new OpenLayers.Marker(ourpoint)); 
+        map_1.setCenter (ourpoint, zoom);
+
+//    map_1 = crearMapa('map_1');
+//    var lonLat = new OpenLayers.LonLat(position.coords.longitude,position.coords.latitude).transform(
+//        new OpenLayers.Projection( 'EPSG:4326' ),
+//        map_1.getProjectionObject() );
+//    map_1.addLayer(new OpenLayers.Layer.OSM());
+//    
 //    var zoom=16;
 //    var markers = new OpenLayers.Layer.Markers( "Markers" );
-//    map.addLayer(markers); 
-//    markers.addMarker(new OpenLayers.Marker(ourpoint)); 
-//    map.setCenter (ourpoint, zoom);
+//    map_1.addLayer(markers);
+//    markers.addMarker(new OpenLayers.Marker(lonLat)); 
+//    map_1.setCenter ( lonLat, zoom );
 }
--- zeus/php/guardarDatos.php
+++ zeus/php/guardarDatos.php
@@ -1,14 +1,20 @@
 <?php
+
 /**
  * Guardar los datos
  */
 require_once('../dll/conect.php');
 extract($_GET);
 
-$sql = "INSERT INTO sitios (nombre,direccion,telefono,precio,latitud,longitud,foto) 
-VALUES ('$nombre','$direccion','$telefono',$precio,$latitud,$longitud,'$foto')";
+if (isset($nombre)) {
+    $sql = "INSERT INTO sitios (nombre,direccion,telefono,precio,latitud,longitud,tipo,estado,mail_contacto) 
+            VALUES ('$nombre','$direccion','$telefono',$precio,$lat,$lon,'$tipo',0,'$mail')";
 
-consulta($sql);
+    consulta($sql);
+    
+}
 
-echo true;
+//echo "<script type=\"text/javascript\">window.location = \"../index.php\"</script>";
+header("Location: ../index.php");
+exit();  // you need to exit after a Location header is sent
 ?>
